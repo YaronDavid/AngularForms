cc197b8b4426b5a09d47ef16d923d88429ca6112
YaronDavid <urlof_desty91@hotmail.com>
2023-04-14 10:22:31 -0600

    Template Form

--- src/app/fomulario-template/fomulario-template/fomulario-template.component.ts
+++ src/app/fomulario-template/fomulario-template/fomulario-template.component.ts
@@ -11,7 +11,47 @@ export class FomularioTemplateComponent {
   @ViewChild("miFormulario")
   miFormulario!:NgForm
 
-  agregar(){
-    console.log("Enviado correctamente");
+  tecnologias:string[] = []
+
+  initialState = {
+    proyecto: "primero",
+    horas: 0,
+    tecnologia: "lo que sea bueno"
   }
+
+  proyectos:any[] = []
+
+  agregar(){
+    if(this.miFormulario.invalid){
+      return;
+    }
+    this.proyectos.push({
+      proyecto: this.miFormulario.controls["proyecto"].value,
+      horas: this.miFormulario.controls["horas"].value,
+      tecnologias : this.tecnologias
+    });
+
+    this.miFormulario.resetForm()
+    this.tecnologias = []
+
+    console.log(this.proyectos);
+
+  }
+
+  agregarTec(){
+
+    if(this.miFormulario.controls["tecnologia"].invalid){
+      return;
+    }
+
+
+    this.tecnologias.push(this.miFormulario.controls["tecnologia"].value);
+
+    this.miFormulario.resetForm({
+      ...this.miFormulario.value,
+      tecnologia: ""
+    })
+
+  }
+
 }
