d4004dd45c070abd7b2b634c86c48e223f08cdfe
YaronDavid <urlof_desty91@hotmail.com>
2023-04-14 12:59:39 -0600

    testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormularioReactiveComponent } from './formulario-reactive/formulario-reactive/formulario-reactive.component';
+import { FomularioTemplateComponent } from './fomulario-template/fomulario-template/fomulario-template.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        FormularioReactiveComponent,
+        FomularioTemplateComponent
       ],
     }).compileComponents();
   });
@@ -14,18 +18,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'formularios'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('formularios');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('formularios app is running!');
   });
 });
--- src/app/formulario-reactive/formulario-reactive/formulario-reactive.component.spec.ts
+++ src/app/formulario-reactive/formulario-reactive/formulario-reactive.component.spec.ts
@@ -1,23 +1,79 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormBuilder } from '@angular/forms';
 import { FormularioReactiveComponent } from './formulario-reactive.component';
 
 describe('FormularioReactiveComponent', () => {
   let component: FormularioReactiveComponent;
-  let fixture: ComponentFixture<FormularioReactiveComponent>;
+
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ FormularioReactiveComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(FormularioReactiveComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new FormularioReactiveComponent(new FormBuilder())
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('deberia crear un formulario con 3 campos, proyecto, horas y tecnologias', () => {
+
+    const formulario = component.miFormulario
+
+    const proyecto = formulario.get("proyecto")
+    const horas = formulario.get("horas")
+    const tecnologias = formulario.get("tecnologias")
+
+    expect(proyecto).toBeTruthy();
+    expect(horas).toBeTruthy();
+    expect(tecnologias).toBeTruthy();
+  });
+  it("deberia verificar que el campo proyecto acepte minimo 3 letras", ()=>{
+    const formulario = component.miFormulario
+
+    const proyecto = formulario.get("proyecto")
+    proyecto?.setValue("abc")
+
+    expect(proyecto?.valid).toBeTruthy();
+  })
+
+  it("deberia verificar que el campo proyecto acepte maximo 30 letras", ()=>{
+    const formulario = component.miFormulario
+
+    const proyecto = formulario.get("proyecto")
+    proyecto?.setValue("abccmzbxmbcjhabtsresds")
+
+    expect(proyecto?.valid).toBeTruthy();
+  })
+  it("deberia verificar que el campo horas", ()=>{
+    const formulario = component.miFormulario
+
+    const horas = formulario.get("horas")
+
+    expect(horas?.valid).toBeTruthy();
+  })
+
+  it("deberia verificar que el campo horas", ()=>{
+    const formulario = component.miFormulario
+
+    const horas = formulario.get("horas")
+    horas?.setValue(0)
+
+    expect(horas?.valid).toBeTruthy();
+  })
+  it("deberia verificar que el campo horas invalido", ()=>{
+    const formulario = component.miFormulario
+
+    const horas = formulario.get("horas")
+    horas?.setValue(200)
+
+    expect(horas?.invalid).toBeTruthy();
+  })
+
+  it("deberia verificar que el campo horas invalido", ()=>{
+    const tecnologia = component.tecnologia
+    const tecnologias = component.tecnologias
+
+    tecnologia.setValue("node")
+    component.agregarTec();
+    expect(tecnologias.length).toBe(1)
+    expect(tecnologias?.value).toEqual(['node'])
+  })
+
 });
